2434a6e321756685bd33f13fdf2661a42df9ef19
Luciano Garbarino <lucigar01@gmail.com>
2022-04-28 17:51:28 -0300

    cvh

--- found_reader.py
+++ found_reader.py
@@ -7,68 +7,81 @@ from time import sleep
 # formato: "fondo", "url", $patrimonio, %mirg, $mirg
 fondos = [
 
-    ["1822 Raices Valores Negociables", "https://www.cafci.org.ar/ficha-fondo.html?q=41;41", -1,-1,-1],
-    ["Alpha Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=35;35", -1,-1,-1 ],
-    ["Alpha Mega", "https://www.cafci.org.ar/ficha-fondo.html?q=36;381", -1,-1,-1],
-    ["Balanz", "https://www.cafci.org.ar/ficha-fondo.html?q=669;1400", -1,-1,-1],
-    ["Bull Market", "https://www.cafci.org.ar/ficha-fondo.html?q=847;2408", -1,-1,-1],
-    ["Consultatio Acciones Argentina", "https://www.cafci.org.ar/ficha-fondo.html?q=216;1208", -1,-1,-1],
-    ["Consultatio Renta Variable", "https://www.cafci.org.ar/ficha-fondo.html?q=514;1037", -1,-1,-1],
-    ["Fima Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=21;21", -1,-1,-1],
-    ["Fima PB Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=22;22", -1,-1,-1],
-    ["HF Acciones Argentinas", "https://www.cafci.org.ar/ficha-fondo.html?q=164;164", -1,-1,-1],
-    ["HF Acciones Lideres", "https://www.cafci.org.ar/ficha-fondo.html?q=29;29", -1,-1,-1],
-    ["Quinquela Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=378;1214", -1,-1,-1],
-    ["SBS Acciones Argentina", "https://www.cafci.org.ar/ficha-fondo.html?q=436;820", -1,-1,-1],
-    ["Rofex 20 Renta Variable", "https://www.cafci.org.ar/ficha-fondo.html?q=599;1216", -1,-1,-1],
-    ["FBA Acciones Argentinas", "https://www.cafci.org.ar/ficha-fondo.html?q=433;814", -1,-1,-1],
-    ["FBA Calificado", "https://www.cafci.org.ar/ficha-fondo.html?q=200;200", -1,-1,-1],
-    ["Pellegrini Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=118;118", -1,-1,-1],
-    ["Pionero Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=39;39", -1,-1,-1],
-    ["Toronto Trust Multimercado", "https://www.cafci.org.ar/ficha-fondo.html?q=561;1135", -1,-1,-1],
-    ["IAM Renta Variable", "https://www.cafci.org.ar/ficha-fondo.html?q=430;803", -1,-1,-1],
-    ["Allaria Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=441;835", -1,-1,-1],
-    ["Premier Renta Variable", "https://www.cafci.org.ar/ficha-fondo.html?q=227;227", -1,-1,-1],
-    ["1810 Renta variable", "https://www.cafci.org.ar/ficha-fondo.html?q=275;275", -1,-1,-1],
-    ["Adcap"," https://www.cafci.org.ar/ficha-fondo.html?q=622;1255", -1,-1,-1],
-    ["Argenfunds", "https://www.cafci.org.ar/ficha-fondo.html?q=880;2544", -1,-1,-1],
-    ["Gainvest", "https://www.cafci.org.ar/ficha-fondo.html?q=510;1031", -1,-1,-1],
-    ["Goal", "https://www.cafci.org.ar/ficha-fondo.html?q=6;680", -1,-1,-1],
-    ["IEB","https://www.cafci.org.ar/ficha-fondo.html?q=890;2592", -1,-1,-1],
-    ["Megainver","https://www.cafci.org.ar/ficha-fondo.html?q=613;1237", -1,-1,-1],
-   
-    
+    ["1822 Raices Valores Negociables",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=41;41", -1, -1, -1],
+    ["Alpha Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=35;35", -1, -1, -1],
+    ["Alpha Mega", "https://www.cafci.org.ar/ficha-fondo.html?q=36;381", -1, -1, -1],
+    ["Balanz", "https://www.cafci.org.ar/ficha-fondo.html?q=669;1400", -1, -1, -1],
+    ["Bull Market", "https://www.cafci.org.ar/ficha-fondo.html?q=847;2408", -1, -1, -1],
+    ["Consultatio Acciones Argentina",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=216;1208", -1, -1, -1],
+    ["Consultatio Renta Variable",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=514;1037", -1, -1, -1],
+    ["Fima Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=21;21", -1, -1, -1],
+    ["Fima PB Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=22;22", -1, -1, -1],
+    ["HF Acciones Argentinas",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=164;164", -1, -1, -1],
+    ["HF Acciones Lideres", "https://www.cafci.org.ar/ficha-fondo.html?q=29;29", -1, -1, -1],
+    ["Quinquela Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=378;1214", -1, -1, -1],
+    ["SBS Acciones Argentina",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=436;820", -1, -1, -1],
+    ["Rofex 20 Renta Variable",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=599;1216", -1, -1, -1],
+    ["FBA Acciones Argentinas",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=433;814", -1, -1, -1],
+    ["FBA Calificado", "https://www.cafci.org.ar/ficha-fondo.html?q=200;200", -1, -1, -1],
+    ["Pellegrini Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=118;118", -1, -1, -1],
+    ["Pionero Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=39;39", -1, -1, -1],
+    ["Toronto Trust Multimercado",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=561;1135", -1, -1, -1],
+    ["IAM Renta Variable", "https://www.cafci.org.ar/ficha-fondo.html?q=430;803", -1, -1, -1],
+    ["Allaria Acciones", "https://www.cafci.org.ar/ficha-fondo.html?q=441;835", -1, -1, -1],
+    ["Premier Renta Variable",
+        "https://www.cafci.org.ar/ficha-fondo.html?q=227;227", -1, -1, -1],
+    ["1810 Renta variable", "https://www.cafci.org.ar/ficha-fondo.html?q=275;275", -1, -1, -1],
+    ["Adcap", " https://www.cafci.org.ar/ficha-fondo.html?q=622;1255", -1, -1, -1],
+    ["Argenfunds", "https://www.cafci.org.ar/ficha-fondo.html?q=880;2544", -1, -1, -1],
+    ["Gainvest", "https://www.cafci.org.ar/ficha-fondo.html?q=510;1031", -1, -1, -1],
+    ["Goal", "https://www.cafci.org.ar/ficha-fondo.html?q=6;680", -1, -1, -1],
+    ["IEB", "https://www.cafci.org.ar/ficha-fondo.html?q=890;2592", -1, -1, -1],
+    ["Megainver", "https://www.cafci.org.ar/ficha-fondo.html?q=613;1237", -1, -1, -1],
+
+
 
 ]
 
-#load parameters
+# load parameters
 
 start_time = 15
-errores = [0,]
+errores = [0, ]
 
-FCI_Bot = FCI_Bot(fondos,start_time)
+FCI_Bot = FCI_Bot(fondos, start_time)
 FCI_Bot._loadParameters_(fondos[0][1])
 
-#Scrapping Websites
+# Scrapping Websites
 
 if FCI_Bot.choice == 1:
     for fondo in FCI_Bot.fondos:
-        fondo[2],fondo[3] = FCI_Bot._getDataM_(fondo[1])
+        fondo[2], fondo[3] = FCI_Bot._getDataM_(fondo[1])
 
 elif FCI_Bot.choice == 2:
     for fondo in FCI_Bot.fondos:
-        fondo[2],fondo[3] = FCI_Bot._getDataY_(fondo[1])
+        fondo[2], fondo[3] = FCI_Bot._getDataY_(fondo[1])
 
 elif FCI_Bot.choice == 3:
     for fondo in FCI_Bot.fondos:
-        fondo[2],fondo[3] = FCI_Bot._getDataV_(fondo[1])
+        fondo[2], fondo[3] = FCI_Bot._getDataV_(fondo[1])
+
+elif FCI_Bot.choice == 4:
+    for fondo in FCI_Bot.fondos:
+        fondo[2], fondo[3] = FCI_Bot._getDataC_(fondo[1])
 
 else:
     print("Error en choice")
     exit()
 
 FCI_Bot.driver.close()
- 
+
 # Data Math
 
 for fondo in FCI_Bot.fondos:
@@ -81,18 +94,18 @@ for fondo in FCI_Bot.fondos:
 
 FCI_Bot._writeExcel_()
 
-#error manager
+# error manager
 for fondo in FCI_Bot.fondos:
     if fondo[3] < 0:
         errores[0] = errores[0] + 1
         errores.append(fondo[0])
 
-log_file = open("log.txt","w")
+log_file = open("log.txt", "w")
 if errores[0] == 0:
     log_file.write("Sin errores")
 
 else:
-    log_file.write("Hay " + str(errores[0])+ " errores en los siguientes fondos: ")
+    log_file.write("Hay " + str(errores[0]) +
+                   " errores en los siguientes fondos: ")
     errores.pop(0)
     log_file.writelines(errores)
-    
--- mylib/func.py
+++ mylib/func.py
@@ -4,23 +4,20 @@ from selenium import webdriver
 from time import sleep
 import xlsxwriter
 
+
 class FCI_Bot:
 
-    def __init__(self,fondos,time):
+    def __init__(self, fondos, time):
 
         self.driver = webdriver.Firefox()
         sleep(3)
 
-        
-        
         self.time = time
         self.fondos = fondos
         self.choice = 0
-        self.fecha = "NaN"    
+        self.fecha = "NaN"
 
-    def _getDataM_ (self,url):
-
-
+    def _getDataM_(self, url):
 
         self.driver.get(url)
         sleep(self.time)
@@ -28,33 +25,34 @@ class FCI_Bot:
         sleep(self.time)
         self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(self.time)
-    
-        
-        html = self.driver.execute_script("return document.documentElement.outerHTML")
+
+        html = self.driver.execute_script(
+            "return document.documentElement.outerHTML")
         sel_soup = BeautifulSoup(html, "html.parser")
 
         datos_crudos = sel_soup.findAll("tr")
         datos_crudos = str(datos_crudos)
 
-
-        posStart = datos_crudos.find("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
-        tamañoStart = len("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""") 
-        posEnd = datos_crudos.find("""</strong></td><!-- end ngIf: row.tipo == 'pie'""")
+        posStart = datos_crudos.find(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        tamañoStart = len(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        posEnd = datos_crudos.find(
+            """</strong></td><!-- end ngIf: row.tipo == 'pie'""")
         Patrimonio = datos_crudos[posStart+tamañoStart:posEnd]
-        Patrimonio = Patrimonio.replace(",","")
-
+        Patrimonio = Patrimonio.replace(",", "")
 
         if Patrimonio == "":
             print("Error en string (getPatrimonio)")
             return -1
-        
+
         try:
             Patrimonio = float(Patrimonio)
 
         except:
             print("Error en conversion a float (getPatrimonio)")
             Patrimonio = -1
-            
+
         posStart = datos_crudos.find("Mirgor")
         Mirg = datos_crudos[posStart+141:]
 
@@ -70,11 +68,9 @@ class FCI_Bot:
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
 
-        return (Patrimonio,Mirg)
-            
-    def _getDataY_ (self,url):
+        return (Patrimonio, Mirg)
 
-
+    def _getDataY_(self, url):
 
         self.driver.get(url)
         sleep(self.time)
@@ -82,33 +78,34 @@ class FCI_Bot:
         sleep(self.time)
         self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(self.time)
-    
-        
-        html = self.driver.execute_script("return document.documentElement.outerHTML")
+
+        html = self.driver.execute_script(
+            "return document.documentElement.outerHTML")
         sel_soup = BeautifulSoup(html, "html.parser")
 
         datos_crudos = sel_soup.findAll("tr")
         datos_crudos = str(datos_crudos)
 
-
-        posStart = datos_crudos.find("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
-        tamañoStart = len("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""") 
-        posEnd = datos_crudos.find("""</strong></td><!-- end ngIf: row.tipo == 'pie'""")
+        posStart = datos_crudos.find(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        tamañoStart = len(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        posEnd = datos_crudos.find(
+            """</strong></td><!-- end ngIf: row.tipo == 'pie'""")
         Patrimonio = datos_crudos[posStart+tamañoStart:posEnd]
-        Patrimonio = Patrimonio.replace(",","")
-
+        Patrimonio = Patrimonio.replace(",", "")
 
         if Patrimonio == "":
             print("Error en string (getPatrimonio)")
             return -1
-        
+
         try:
             Patrimonio = float(Patrimonio)
 
         except:
             print("Error en conversion a float (getPatrimonio)")
             Patrimonio = -1
-            
+
         posStart = datos_crudos.find("YPF - D")
         YPF = datos_crudos[posStart+142:]
 
@@ -123,42 +120,43 @@ class FCI_Bot:
 
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
-        return (Patrimonio,YPF)
+        return (Patrimonio, YPF)
 
-    def _getDataV_(self,url):
+    def _getDataV_(self, url):
         self.driver.get(url)
         sleep(self.time)
         self.driver.find_element_by_class_name("html").click()
         sleep(self.time)
         self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(self.time)
-    
-        
-        html = self.driver.execute_script("return document.documentElement.outerHTML")
+
+        html = self.driver.execute_script(
+            "return document.documentElement.outerHTML")
         sel_soup = BeautifulSoup(html, "html.parser")
 
         datos_crudos = sel_soup.findAll("tr")
         datos_crudos = str(datos_crudos)
 
-
-        posStart = datos_crudos.find("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
-        tamañoStart = len("""<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""") 
-        posEnd = datos_crudos.find("""</strong></td><!-- end ngIf: row.tipo == 'pie'""")
+        posStart = datos_crudos.find(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        tamañoStart = len(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        posEnd = datos_crudos.find(
+            """</strong></td><!-- end ngIf: row.tipo == 'pie'""")
         Patrimonio = datos_crudos[posStart+tamañoStart:posEnd]
-        Patrimonio = Patrimonio.replace(",","")
-
+        Patrimonio = Patrimonio.replace(",", "")
 
         if Patrimonio == "":
             print("Error en string (getPatrimonio)")
             return -1
-        
+
         try:
             Patrimonio = float(Patrimonio)
 
         except:
             print("Error en conversion a float (getPatrimonio)")
             Patrimonio = -1
-            
+
         posStart = datos_crudos.find("Cedear Vista Oil Gas")
         VIS = datos_crudos[posStart+155:]
 
@@ -173,9 +171,60 @@ class FCI_Bot:
 
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
-        return (Patrimonio,VIS)
+        return (Patrimonio, VIS)
 
-    def _loadParameters_(self,url):
+    def _getDataC_(self, url):
+        self.driver.get(url)
+        sleep(self.time)
+        self.driver.find_element_by_class_name("html").click()
+        sleep(self.time)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        sleep(self.time)
+
+        html = self.driver.execute_script(
+            "return document.documentElement.outerHTML")
+        sel_soup = BeautifulSoup(html, "html.parser")
+
+        datos_crudos = sel_soup.findAll("tr")
+        datos_crudos = str(datos_crudos)
+
+        posStart = datos_crudos.find(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        tamañoStart = len(
+            """<!-- ngIf: row.tipo == 'pie' --><td class="ng-scope" ng-if="row.tipo == 'pie'"><strong class="ng-binding">""")
+        posEnd = datos_crudos.find(
+            """</strong></td><!-- end ngIf: row.tipo == 'pie'""")
+        Patrimonio = datos_crudos[posStart+tamañoStart:posEnd]
+        Patrimonio = Patrimonio.replace(",", "")
+
+        if Patrimonio == "":
+            print("Error en string (getPatrimonio)")
+            return -1
+
+        try:
+            Patrimonio = float(Patrimonio)
+
+        except:
+            print("Error en conversion a float (getPatrimonio)")
+            Patrimonio = -1
+
+        posStart = datos_crudos.find("Cablevision Holding CL. B")
+        CVH = datos_crudos[posStart+160:]
+
+        posEnd = CVH.find("<")
+        CVH = CVH[:posEnd]
+
+        try:
+            CVH = float(CVH)
+        except:
+            print("Error conversion a float getCVH")
+            CVH = -1
+
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        return (Patrimonio, CVH)
+
+    def _loadParameters_(self, url):
 
         self.driver.get(url)
         sleep(self.time*2)
@@ -183,39 +232,43 @@ class FCI_Bot:
         sleep(self.time*2)
         self.driver.switch_to.window(self.driver.window_handles[1])
         sleep(self.time*2)
-        
-            
-        html = self.driver.execute_script("return document.documentElement.outerHTML")
+
+        html = self.driver.execute_script(
+            "return document.documentElement.outerHTML")
         sel_soup = BeautifulSoup(html, "html.parser")
 
         datos_crudos = sel_soup.findAll("p")
         datos_crudos = str(datos_crudos)
 
-        posStart = datos_crudos.find("""<p class="encuentreColortxt ng-binding">1822 Raices Valores Negociables<br/>Composición de Cartera al """)
-        tamañoStart = len("""<p class="encuentreColortxt ng-binding">1822 Raices Valores Negociables<br/>Composición de Cartera al""") 
-        posEnd = datos_crudos.find(""" </p>, <p class="destacado ng-binding">ARS 1000</p>, <p class="destacado ng-binding">48 hs. hábiles</p>""")
+        posStart = datos_crudos.find(
+            """<p class="encuentreColortxt ng-binding">1822 Raices Valores Negociables<br/>Composición de Cartera al """)
+        tamañoStart = len(
+            """<p class="encuentreColortxt ng-binding">1822 Raices Valores Negociables<br/>Composición de Cartera al""")
+        posEnd = datos_crudos.find(
+            """ </p>, <p class="destacado ng-binding">ARS 1000</p>, <p class="destacado ng-binding">48 hs. hábiles</p>""")
         self.fecha = datos_crudos[posStart+tamañoStart:posEnd]
 
         self.driver.close()
         sleep(1)
         self.driver.switch_to.window(self.driver.window_handles[0])
-        
 
-        print("La fecha a analizar es: ",self.fecha)
+        print("La fecha a analizar es: ", self.fecha)
 
         while True:
-            
 
             try:
-                self.choice = int(input("Ingrese\n 1 --> MIRG\n 2 --> YPF\n 3 --> VIST\n Su eleccion: "))
-                self.COT = float(input("Ingrese la cotizacion del Activo en el periodo a analizar (999.99): "))
-                self.time = float(input("Ingrese la velocidad de funcionamiento, recomendable 8.0 (sg.ms): "))
+                self.choice = int(input(
+                    "Ingrese\n 1 --> MIRG\n 2 --> YPF\n 3 --> VIST\n 4 --> CVH\n Su eleccion: "))
+                self.COT = float(
+                    input("Ingrese la cotizacion del Activo en el periodo a analizar (999.99): "))
+                self.time = float(
+                    input("Ingrese la velocidad de funcionamiento, recomendable 8.0 (sg.ms): "))
                 break
             except:
                 print("Revise los formatos de lo que ingreso")
-        
+
     def _writeExcel_(self):
-    
+
         if self.choice == 1:
             outWorkbook = xlsxwriter.Workbook("outMirg.xlsx")
             outSheet = outWorkbook.add_worksheet()
@@ -227,7 +280,7 @@ class FCI_Bot:
             for item in range(len(self.fondos)):
                 outSheet.write(item+2, 0, self.fondos[item][0])
                 outSheet.write(item+2, 2, self.fondos[item][4])
-    
+
         elif self.choice == 2:
             outWorkbook = xlsxwriter.Workbook("outYPF.xlsx")
             outSheet = outWorkbook.add_worksheet()
@@ -239,7 +292,7 @@ class FCI_Bot:
             for item in range(len(self.fondos)):
                 outSheet.write(item+2, 0, self.fondos[item][0])
                 outSheet.write(item+2, 2, self.fondos[item][4])
-        
+
         elif self.choice == 3:
             outWorkbook = xlsxwriter.Workbook("outVist.xlsx")
             outSheet = outWorkbook.add_worksheet()
@@ -251,11 +304,21 @@ class FCI_Bot:
             for item in range(len(self.fondos)):
                 outSheet.write(item+2, 0, self.fondos[item][0])
                 outSheet.write(item+2, 2, self.fondos[item][4])
-        
+
+        elif self.choice == 4:
+            outWorkbook = xlsxwriter.Workbook("outCVH.xlsx")
+            outSheet = outWorkbook.add_worksheet()
+
+            outSheet.write("A2", "Fondos")
+            outSheet.write("C2", "Cant. CVH")
+            outSheet.write("B1", self.fecha)
+
+            for item in range(len(self.fondos)):
+                outSheet.write(item+2, 0, self.fondos[item][0])
+                outSheet.write(item+2, 2, self.fondos[item][4])
+
         else:
             print("Error en choice")
             exit()
 
-
         outWorkbook.close()
-        
